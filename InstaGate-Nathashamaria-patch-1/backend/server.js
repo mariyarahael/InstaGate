cdff43ecfcf9035ad5574d91243d15d38a068b29
Nathashamaria <marianathashabijumon@gmail.com>
2025-02-28 21:00:36 +0530

    studentportal

---
+++ InstaGate-Nathashamaria-patch-1/backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://instagate100820:MarNatIrin@cluster0.tbtgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// Define Schema & Model
+const DataSchema = new mongoose.Schema({
+  name: String,
+  message: String,
+});
+
+const DataModel = mongoose.model("Data", DataSchema);
+
+// API Routes
+app.get("/data", async (req, res) => {
+  const data = await DataModel.find();
+  res.json(data);
+});
+
+app.post("/data", async (req, res) => {
+  const newData = new DataModel(req.body);
+  await newData.save();
+  res.json({ message: "Data saved!" });
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
